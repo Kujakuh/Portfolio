ee57910774ed14c087eac7bb69bbcd412ccef4c2
Alvaro Vos <avos202@gmail.com>
2024-05-08 12:09:50 +0200

    Revert "Init commit"
    This reverts commit 420c536da7faeb6afdc9c6843145ecb308e75312.

--- src/main/java/repos/ContactMeRepository.java
+++
@@ -1,9 +0,0 @@
-package repos;
-
-import model.ContactMe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactMeRepository extends JpaRepository<ContactMe, Long> {
-}
--- src/main/java/repos/HeaderRepository.java
+++
@@ -1,9 +0,0 @@
-package repos;
-
-import model.Header;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HeaderRepository extends JpaRepository<Header, Long> {
-}
